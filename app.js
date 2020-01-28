27a4446fa36d5bb23d1b4a2933fbfd3170997c01
yibinouyang <43450746+yibinouyang@users.noreply.github.com>
2020-01-28 15:44:29 -0500

    inclass 2
    second part

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+
+const io = require('socket.io')();//instantiate the library right way with the ()method-> makes it run
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -13,3 +16,17 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+//this is all of our socket.io messages functionality
+
+//attach socket.io
+io.attach(server);
+
+io.on('connection',function(socket){
+    console.log('user connected');
+
+    //listen for a disconnect event
+    socket.on('disconnect',function(){
+        console.log('a user disconnected');
+    })
+})
